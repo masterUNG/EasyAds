a4acd3dfe517cabe43818504e62a0f312ec0fe48
MasterUNG <phrombutr@gmail.com>
2017-10-10 14:24:42 +0700

    Add Fragment Success

--- app/src/main/java/app/ewtc/masterung/easyads/MainActivity.java
+++ app/src/main/java/app/ewtc/masterung/easyads/MainActivity.java
@@ -3,12 +3,23 @@ package app.ewtc.masterung.easyads;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import app.ewtc.masterung.easyads.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Add Fragment to Activity
+        if (savedInstanceState == null){
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentFragmentMain, new MainFragment()).commit();
+
+        }
+
     }   // Main Method
 
 }   // Main Class
